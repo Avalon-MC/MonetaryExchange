98d9f89edd12c584774fd700a664466847cd4962
Peter Cashel <pacas00@gmail.com>
2025-04-26 08:04:18 +1000

    check in

--- src/main/java/net/petercashel/monetaryexchange/items/ItemCoin.java
+++ src/main/java/net/petercashel/monetaryexchange/items/ItemCoin.java
@@ -1,9 +1,16 @@
 package net.petercashel.monetaryexchange.items;
 
+import net.minecraft.Util;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+
+import javax.annotation.Nullable;
 
 public class ItemCoin extends Item {
     private final String CurrencyGameID;
+    @Nullable
+    private String descriptionId;
 
     public ItemCoin(Properties properties, String currencyGameID) {
         super(properties);
--- src/main/java/net/petercashel/monetaryexchange/items/ItemWallet.java
+++ src/main/java/net/petercashel/monetaryexchange/items/ItemWallet.java
@@ -1,6 +1,14 @@
 package net.petercashel.monetaryexchange.items;
 
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 
 public class ItemWallet extends Item {
     private final String CurrencyGameID;
@@ -12,5 +20,20 @@ public class ItemWallet extends Item {
 
     public boolean IsCurrencyGameID(String currencyGameID) {
         return currencyGameID.equals(CurrencyGameID);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        return super.useOn(context);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        return super.use(level, player, usedHand);
+    }
+
+    @Override
+    public InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity interactionTarget, InteractionHand usedHand) {
+        return super.interactLivingEntity(stack, player, interactionTarget, usedHand);
     }
 }
